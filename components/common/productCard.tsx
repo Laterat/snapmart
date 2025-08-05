9c8267a413f0ffdfd5ffb0d2ef1629c7748767fb
Latera Mekonnen <mekonnenlatera@gmail.com>
2025-08-05 18:50:29 +0300

    feat: add productCard

--- components/common/productCard.tsx
+++ components/common/productCard.tsx
@@ -1,7 +1,36 @@
+import { ProductCardProps } from "@/interfaces";
+import Image from "next/image";
+import { FaStar } from "react-icons/fa";
 
+const ProductCard: React.FC<ProductCardProps> = ({
+  id,
+  title,
+  price,
+  discountPercentage,
+  rating,
+  images,
+}) => {
+  return (
+    <section>
+      <div>
+        <div className="relative w-full h-auto aspect-[380/255] ">
+          <Image src={images} alt={title} fill className="rounded-2xl z-0" />
 
-const Card:React.FC=()=>{
-    return(
-        
-    );
-}
+          {Number(discountPercentage) > 0 && (
+            <div className="absolute top-5 rounded-bl-none bg-[#15a17c] text-[#EDEDED] text-xl font-bold px-2 py-2 rounded-full shadow-2xl z-20 ">
+              {discountPercentage}% off
+            </div>
+          )}
+        </div>
+        <p>{title}</p>
+        <div>
+          <p>{price}</p>
+          <p>
+            <FaStar className="text-yellow-500" />
+            <span>{rating}</span>
+          </p>
+        </div>
+      </div>
+    </section>
+  );
+};
--- interfaces/index.ts
+++ interfaces/index.ts
@@ -25,7 +25,7 @@ export interface ProductProps {
   returnPolicy: string;
   minimumOrderQuantity: number;
   meta: Meta;
-  images: string[];
+  images: string;
   thumbnail: string;
 }
 
@@ -49,3 +49,6 @@ export interface Review {
   reviewerName: string;
   reviewerEmail: string;
 }
+
+
+export type ProductCardProps = Pick<ProductProps, 'id' | 'title' | 'price' | 'discountPercentage' | 'rating'| 'images'>;
