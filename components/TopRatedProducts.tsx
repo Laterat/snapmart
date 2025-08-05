8705b9584e0deecfb515bf31f9e343785fb33c1b
Latera Mekonnen <mekonnenlatera@gmail.com>
2025-08-05 21:52:00 +0300

    style: ui for cards and top rated title

--- components/TopRatedProducts.tsx
+++ components/TopRatedProducts.tsx
@@ -12,12 +12,10 @@ const TopRatedProducts: React.FC = () => {
   useEffect(() => {
     const fetchProducts = async () => {
       try {
-        const res = await axios.get(
-          "https://dummyjson.com/products"
-        );
+        const res = await axios.get("https://dummyjson.com/products");
         const sorted = res.data.products
           .sort((a: ProductProps, b: ProductProps) => b.rating - a.rating)
-          .slice(0, 6);
+          .slice(0, 8);
         setTopProducts(sorted);
       } catch (err) {
         console.error("Failed to fetch products", err);
@@ -29,25 +27,27 @@ const TopRatedProducts: React.FC = () => {
   }, []);
 
   return (
-    <section className="mt-7 mx-10">
-      <h1 className="text-2xl mb-5">Top Rated Products</h1>
-        {loading ? (
-    <p>Loading...</p>
-  ) : (
-    <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-6">
-      {topProducts.map((product) => (
-        <ProductCard
-          key={product.id}
-          id={product.id}
-          images={product.images}
-          title={product.title}
-          price={product.price}
-          rating={product.rating}
-          discountPercentage={product.discountPercentage}
-        />
-      ))}
-    </div>
-  )}
+    <section className="mt-15 mx-10">
+      <h1 className="text-2xl mb-10 md:text-5xl font-extrabold">
+        Top Rated Products
+      </h1>
+      {loading ? (
+        <p>Loading...</p>
+      ) : (
+        <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-6">
+          {topProducts.map((product) => (
+            <ProductCard
+              key={product.id}
+              id={product.id}
+              images={product.images}
+              title={product.title}
+              price={product.price}
+              rating={product.rating}
+              discountPercentage={product.discountPercentage}
+            />
+          ))}
+        </div>
+      )}
     </section>
   );
 };
--- components/common/productCard.tsx
+++ components/common/productCard.tsx
@@ -12,20 +12,26 @@ const ProductCard: React.FC<ProductCardProps> = ({
 }) => {
   return (
     <section>
-      <div>
-        <div className="relative w-full h-auto aspect-[380/255] ">
-          <Image src={images[0]} alt={title} fill className="rounded-2xl z-0" />
+      <div className="shadow-md rounded-2xl transition duration-300 ease-in-out transform hover:shadow-2xl  hover:scale-105 hover:-translate-y-1">
+        <div className="relative w-full h-auto aspect-[380/255] mb-5 ">
+          <Image
+            src={images[0]}
+            alt={title}
+            fill
+            
+            className="rounded-2xl bg-gray-200 "
+          />
 
           {Number(discountPercentage) > 0 && (
-            <div className="absolute top-5  rounded-bl-none bg-[#cd310f] text-[#EDEDED] text-xl font-bold px-2 py-2 rounded-full shadow-2xl z-20 ">
+            <div className="absolute top-4  rounded-bl-none bg-[#cd310f] text-[#EDEDED]  px-2 py-1 rounded-full shadow-2xl ">
               {discountPercentage}% off
             </div>
           )}
         </div>
-        <p>{title}</p>
-        <div className="flex justify-between">
+        <p className="px-4 text-lg font-semibold">{title}</p>
+        <div className="flex justify-between px-5">
           <p className="text-xl">${price}</p>
-          <p className="flex items-center space-x-2 justify-center">
+          <p className="flex items-center space-x-2 justify-center mb-5">
             <FaStar className="text-yellow-500 text-xl" />
             <span className="text-xl">{rating}</span>
           </p>
