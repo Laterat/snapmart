6d0913e076ba4c64e23ffb2f51f9d4579145075f
Latera Mekonnen <mekonnenlatera@gmail.com>
2025-08-09 18:27:44 +0300

    feat: add the cart page

--- pages/cart/index.tsx
+++ pages/cart/index.tsx
@@ -8,12 +8,31 @@ export default function CartPage() {
   const dispatch = useDispatch();
   const { items } = useSelector((state: RootState) => state.cart);
   return (
-    <section>
+    <section className="mt-5 md:mx-20">
       <h1>Cart</h1>
-      {items.map((item)=>(
-        <div>  <p>{items[0].id}</p>;</div>
+         <div className="grid grid-cols-5">
+          <p>S/p>
+          <p>{item.title}</p>
+          <p>{item.price}</p>
+          <p>{item.quantity}</p>
+          <Pill
+            label="remove"
+            onclick={() => dispatch(removeFromCart(item.id))}
+          />
+        </div>
+      {items.map((item) => (
+        <div className="grid grid-cols-5">
+          <p>{items[0].id}</p>
+          <p>{item.title}</p>
+          <p>{item.price}</p>
+          <p>{item.quantity}</p>
+          <Pill
+            label="remove"
+            onclick={() => dispatch(removeFromCart(item.id))}
+          />
+        </div>
       ))}
-    
+
       <div>
         <div>
           <Pill label="Clear Cart" onclick={() => dispatch(clearCart())} />
