0216963d8029c25c19b8cc583a4cef8583b4c8a2
Latera Mekonnen <mekonnenlatera@gmail.com>
2025-08-09 18:17:12 +0300

    style: add active to pill(button component

--- components/common/Pill.tsx
+++ components/common/Pill.tsx
@@ -4,9 +4,9 @@ const Pill: React.FC<pill> = ({ label, onclick }) => {
   return (
     <button
       onClick={onclick}
-      className="mt-4 p-2 bg-gray-200 text-xl rounded-xl px-2 py-2"
+      className="mt-4 p-2 bg-gray-200 text-xl rounded-xl px-2 py-2 hover:bg-gray-300 active:bg-gray-400"
     >
-      {label}
+      {label} 
     </button>
   );
 };
